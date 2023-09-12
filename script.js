2d854790ef67a30edb901fe4d0977ba989859cdb
Marco Estrada <you@example.com>
2023-09-12 14:21:03 -0600

    btn descripcion

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let subcompetenciaSeleccionada = '';
 let posActual = 0;
 let cantidadAcertadas = 0;
 
+function regresarASubcompetencias() {
+    document.getElementById("descripcion").style.display = 'none';
+    document.getElementById("subcompetencias").style.display = 'block';
+}
+
 function mostrarSubcompetencias() {
     document.getElementById("pantallaInitial").style.display = 'none';
     document.getElementById("subcompetencias").style.display = 'block';
