19c6c7fdad3f1a094d0ba9ef894649ce33375ca2
Marco Estrada <you@example.com>
2023-09-12 12:08:25 -0600

    Subcompentencias

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
 // Datos del juego
-let cuestion = ['How do you say? Banana', 'How do you say? Uva', 'How do you say? Naranja'];
-let correcta = [1, 2, 0];
-let opcions = [['Naranja', 'Banana', 'Uva'], ['Naranja', 'Banana', 'Uva'], ['Naranja', 'Banana', 'Uva']];
-
+let cuestion = [];
+let correcta = [];
+let opcions = [];
+let subcompetenciaSeleccionada = '';
 
 // Variables de juego
 let posActual = 0;
@@ -14,13 +13,45 @@ function mostrarSubcompetencias() {
     document.getElementById("subcompetencias").style.display = 'block';
 }
 
-function comenzarGame() {
+function comenzarGame(subcompetencia) {
+    subcompetenciaSeleccionada = subcompetencia;
     posActual = 0;
     cantidadAcertadas = 0;
     document.getElementById("subcompetencias").style.display = 'none';
     document.getElementById("pantallaGame").style.display = 'block';
-    cargarCuestion();
+    cargarPreguntasYOpciones();
 }
+
+function cargarPreguntasYOpciones() {
+    // Aquí debes cargar las preguntas y opciones según la subcompetenciaSeleccionada
+    // Puedes usar un switch o un objeto con las preguntas y opciones por subcompetencia
+    // Por ejemplo:
+    switch (subcompetenciaSeleccionada) {
+        case 'Subcompetencia 1':
+            // Cargar preguntas y opciones para Subcompetencia 1
+            cuestion = ['How do you say?Banana', 'How do you say?Uva'];
+            correcta = [1, 2];
+            opcions = [['Naranja', 'Banana', 'Uva'], ['Naranja', 'Banana', 'Uva']];
+            cargarCuestion(); // Para cargar la primera pregunta
+            break;
+        case 'Subcompetencia 2':
+            // Cargar preguntas y opciones para Subcompetencia 2
+            cuestion = ['How do you say?Uva', 'How do you say?Naranja'];
+            correcta = [2, 0];
+            opcions = [['Naranja', 'Banana', 'Uva'], ['Naranja', 'Banana', 'Uva']];
+            cargarCuestion(); // Para cargar la primera pregunta
+            break;
+        case 'Subcompetencia 3':
+            // Cargar preguntas y opciones para Subcompetencia 3
+            cuestion = ['How do you say?Naranja', 'How do you say?Banana'];
+            correcta = [0, 1];
+            opcions = [['Naranja', 'Banana', 'Uva'], ['Naranja', 'Banana', 'Uva']];
+            cargarCuestion(); // Para cargar la primera pregunta
+            break;
+        // Agrega más casos para otras subcompetencias
+    }
+}
+
 
 function cargarCuestion() {
     if (cuestion.length <= posActual) {
