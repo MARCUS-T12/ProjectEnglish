86ac59b40511d927ea9852523c54a994c554fcae
Croixssants <othodlca@gmail.com>
2023-11-17 15:37:07 -0600

    AllQuestions

--- question.js
+++ question.js
@@ -66,6 +66,283 @@ let questions=[
             "Who's",
             "When"
         ]
+    },
+    {
+        numb:7,
+        question:"Where were you raised --> Can you tell me _______________?",
+        answer:"Where you were raised",
+        options:[
+            "Where you are raise",
+            "Where you were raise",
+            "Where you were raised",
+            "Where you raised"
+        ]
+    },
+    {
+        numb:8,
+        question:"When does your teacher want to retire? --> Do you have any idea ____________?",
+        answer:"When your teacher wants to retire",
+        options:[
+            "When your teacher wants to retire",
+            "When your teacher want to retire",
+            "When your teacher wants retire",
+            "When your teacher want retire"
+        ]
+    },
+    {
+        numb:9,
+        question:"Are your friends into sports? --> I wonder ______________.",
+        answer:"if your friends are into sports",
+        options:[
+            "your friends is into sports",
+            "if your friends is into sports",
+            "your friends are into sports",
+            "if your friends are into sports"
+        ]
+    },
+    {
+        numb:10,
+        question:"When do your parents celebrate their anniversary? --> Do you know ____________?",
+        answer:"When your parents celebrate their annirveresary",
+        options:[
+            "When your parents celebrate annirveresary",
+            "When your parents celebrate their annirveresary",
+            "When your parents celebrates their annirveresary",
+            "When your parents celebrate their anirveresary"
+        ]
+    },
+    {
+        numb:11,
+        question:"Were your brothers and sisters born in this city? --> I'd like to know _______________.",
+        answer:"If your brothers and sisters were born in this city",
+        options:[
+            "If your brothers and sisters was born in this city",
+            "If your brothers and sisters were born in these city",
+            "If your brothers and sisters were born in this city",
+            "If you brothers and sisters were born in this city"
+        ]
+    },
+    {
+        numb:12,
+        question:"A ____ says meow.",
+        answer:"Cat",
+        options:[
+            "Dog",
+            "Cat",
+            "Elephant",
+            "Monkey"
+        ]
+    },
+    {
+        numb:13,
+        question:"I want to see the ____________ at the zoo.",
+        answer:"Elephant",
+        options:[
+            "Dog",
+            "Cat",
+            "Elephant",
+            "Mouse"
+        ]
+    },
+    {
+        numb:14,
+        question:"The ____________ is the king of the jungle.",
+        answer:"Lion",
+        options:[
+            "Monkey",
+            "Lion",
+            "Elephant",
+            "Mouse"
+        ]
+    },
+    {
+        numb:15,
+        question:"Birds have ____________ and they can fly.",
+        answer:"Wings",
+        options:[
+            "Trunk",
+            "Scales",
+            "Hooves",
+            "Wings"
+        ]
+    },
+    {
+        numb:16,
+        question:"My favorite ____________ is a dog.",
+        answer:"Pet",
+        options:[
+            "Pet",
+            "Pets",
+            "Feline",
+            "Felines"
+        ]
+    },
+    {
+        numb:17,
+        question:"She ____________ (watch) TV every evening.",
+        answer:"Watches",
+        options:[
+            "Watch",
+            "Watching",
+            "Watchs",
+            "Watches"
+        ]
+    },
+    {
+        numb:18,
+        question:"They __________ (play) soccer in the park right now.",
+        answer:"Playing",
+        options:[
+            "Play",
+            "Plays",
+            "Playing",
+            "Played"
+        ]
+    },
+    {
+        numb:19,
+        question:"I usually __________ (read) a book before bed.",
+        answer:"Read",
+        options:[
+            "Read",
+            "Reads",
+            "Reading",
+            "Readed"
+        ]
+    },
+    {
+        numb:20,
+        question:"He __________ (study) for his exam at the moment.",
+        answer:"Stuying",
+        options:[
+            "Study",
+            "Studies",
+            "Stuying",
+            "Studied"
+        ]
+    },
+    {
+        numb:21,
+        question:"We ___________ (go) to the beach on weekends.",
+        answer:"Go",
+        options:[
+            "Go",
+            "Goes",
+            "Going",
+            "Went"
+        ]
+    },
+    {
+        numb:22,
+        question:"Have you __________ been to Paris?",
+        answer:"Ever",
+        options:[
+            "Have",
+            "Are",
+            "Were",
+            "Ever"
+        ]
+    },
+    {
+        numb:23,
+        question:"She has lived in New York __________ 2010.",
+        answer:"Since",
+        options:[
+            "For",
+            "Since",
+            "That",
+            "Ever"
+        ]
+    },
+
+    {
+        numb:24,
+        question:"__________ you finished your homework yet",
+        answer:"Have",
+        options:[
+            "Do",
+            "Are",
+            "Have",
+            "Were"
+        ]
+    },
+    {
+        numb:25,
+        question:"They have known each other __________ a long time.",
+        answer:"For",
+        options:[
+            "For",
+            "Since",
+            "Never",
+            "Ever"
+        ]
+    },
+    {
+        numb:26,
+        question:"I've __________ been to a concert with such an amazing atmosphere.",
+        answer:"Never",
+        options:[
+            "For",
+            "Since",
+            "Never",
+            "Have"
+        ]
+    },
+    {
+        numb:27,
+        question:"Have you seen that movie __________?",
+        answer:"Already",
+        options:[
+            "Already",
+            "Yet",
+            "Have",
+            "For"
+        ]
+    },
+    {
+        numb:28,
+        question:"She hasn't finished her book __________.",
+        answer:"Yet",
+        options:[
+            "Already",
+            "Yet",
+            "Have",
+            "For"
+        ]
+    },
+    {
+        numb:29,
+        question:"__________ you visited the new art exhibition?",
+        answer:"Have",
+        options:[
+            "Already",
+            "Yet",
+            "Have",
+            "For"
+        ]
+    },
+    {
+        numb:30,
+        question:"They've __________ eaten at that restaurant.",
+        answer:"Already",
+        options:[
+            "Already",
+            "Yet",
+            "Have",
+            "For"
+        ]
+    },
+    {
+        numb:31,
+        question:"__________ finished your coffee?",
+        answer:"Already",
+        options:[
+            "Already",
+            "Yet",
+            "Have",
+            "For"
+        ]
     }
 
 ]
+
